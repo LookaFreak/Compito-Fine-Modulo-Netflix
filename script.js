3510bc3d695b74a8a7ac15a9b3f143e1875dce26
Alessandro Camarda <letsgameepicfail@gmail.com>
2024-01-18 19:55:56 +0100

    i try to make extraarea

--- script.js
+++ script.js
@@ -15,3 +15,41 @@ carousels.forEach((carousel) => {
         }
     });
 });
+
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// PROVO UN'ALTRO SCIRPT PERCHè NON CAPISCO SE FUNZIONA
+
+// Selezione dell'elemento HTML
+const extraArea = document.getElementById("extraarea");
+
+// Funzione per mostrare la sezione
+function showExtraArea() {
+  extraArea.classList.remove("hidden");
+}
+
+// Funzione per nascondere la sezione
+function hideExtraArea() {
+  extraArea.classList.add("hidden");
+}
+
+// Event listener per la scroll event
+window.addEventListener("scroll", (event) => {
+  // Verifica se la sezione è visibile
+  if (extraArea.classList.contains("hidden")) {
+    // Se sì, mostrala
+    showExtraArea();
+  } else {
+    // Se no, nascondila
+    hideExtraArea();
+  }
+});
